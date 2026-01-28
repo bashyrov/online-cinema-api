9c5f2138c6837878b85e792d4a741bf8360d36a0
Artur Bashyrov <bashyroov@gmail.com>
2026-01-28 18:41:51 +0100

    Added repository for user's crud methods

---
+++ app/repositories/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from core.security import hash_password
+from models import User
+from repositories.user_group import UserGroupRepository
+from schemas.user import UserCreate
+
+
+async def create_user(db: AsyncSession, user: UserCreate):
+    hashed = hash_password(user.password)
+
+    group = await UserGroupRepository.get_by_name(db, "USER")
+    db_user = User(email=user.email, hashed_password=hashed, group_id=group.id)
+
+    db.add(db_user)
+    await db.commit()
+    await db.refresh(db_user)
+    return db_user
+
+async def get_user_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(User).where(User.email == email))
+    return result.scalar_one_or_none()
