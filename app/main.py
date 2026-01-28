22d20f2dacc31d323f621bfee51db8578d2bf85b
Artur Bashyrov <bashyroov@gmail.com>
2026-01-28 22:10:53 +0100

    Added auth_router and prefix to main path.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.routers.online_cinema import router as online_cinema_router
-app = FastAPI()
+from app.routers.auth import router as auth_router
+app = FastAPI(
+    root_path="/api/v1"
+)
 app.include_router(online_cinema_router)
+app.include_router(auth_router, prefix="/auth")
 
