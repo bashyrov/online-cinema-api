f60514c0cff51f9fa22a19b0f96e46912abc6965
Artur Bashyrov <bashyroov@gmail.com>
2026-01-28 17:23:07 +0100

    Updated config to reading env file

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,17 @@
-#env, settings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # DB
+    DATABASE_URL: str  # пример: postgresql+asyncpg://user:pass@localhost:5432/db
+
+    # JWT (позже понадобится)
+    JWT_SECRET: str = "change_me"
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TTL_MIN: int = 15
+    REFRESH_TTL_DAYS: int = 30
+
+
+settings = Settings()
