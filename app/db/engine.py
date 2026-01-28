1c82f4ac868500b0bedf51a2cd71beda790575b0
Artur Bashyrov <bashyroov@gmail.com>
2026-01-28 17:33:31 +0100

    Updated settings import

--- app/db/engine.py
+++ app/db/engine.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from core.config import settings
+from app.core.config import settings
 
 
 engine = create_async_engine(
