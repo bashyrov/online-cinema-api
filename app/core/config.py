7acd4e0b691a145bfa9f03aa54d230dc110af682
Artur Bashyrov <bashyroov@gmail.com>
2026-01-28 22:06:13 +0100

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -4,11 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    # DB
-    DATABASE_URL: str  # пример: postgresql+asyncpg://user:pass@localhost:5432/db
-
-    # JWT (позже понадобится)
-    JWT_SECRET: str = "change_me"
+    DATABASE_URL: str
+    JWT_SECRET: str = "mysecretkey"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TTL_MIN: int = 15
     REFRESH_TTL_DAYS: int = 30
